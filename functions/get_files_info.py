1dbf010370c5ed7de4190d6e9a3a758d371621b3
Ed-Eduardo <joseeduardo@gmail.com.br>
2025-08-01 21:52:09 -0300

    first steps into the directory functions (UNFINISHED)

---
+++ functions/get_files_info.py
@@ -0,0 +1,14 @@
+
+def get_dir_contents(directory): 
+    pass
+
+def get_files_info(working_directory, directory="."):
+    current_dir = os.path.join(working_directory, directory)
+
+    match current_dir:
+        case dir if dir not in working_directory:
+            f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        case dir if os.path.isdir(dir) != True:
+            f'Error: "{directory}" is not a directory'
+        case _: 
+            pass
